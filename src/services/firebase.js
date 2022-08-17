da85af83324d0aff7b28607cac528cbe06e4675b
Alyzen Jeraj <38901318+alyzenjeraj@users.noreply.github.com>
2022-08-16 20:14:58 -0600

    Initial Commit

---
+++ src/services/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAixxnG8BPy7afUa0_Q2hZOyRjFh2t1HH4",
+  authDomain: "chatrooms-d3e22.firebaseapp.com",
+  projectId: "chatrooms-d3e22",
+  storageBucket: "chatrooms-d3e22.appspot.com",
+  messagingSenderId: "1063197533227",
+  appId: "1:1063197533227:web:82cee12df2f4d0843d766e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
